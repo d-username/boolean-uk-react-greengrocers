92b3fb772d33ead8e91e2998ef795aff3a1ccd92
David <czdavid93@gmail.com>
2022-05-12 16:08:01 +0200

    extension 1 completed: filter and sort works

--- src/Header.js
+++ src/Header.js
@@ -8,38 +8,25 @@ function Header(props) {
   const [storeItems, setStoreItems] = useState(initialStoreItems)
   const [filterBy, setFilterBy] = useState('')
   const [sortBy, setSortBy] = useState('')
-  console.log('sort by what? ', sortBy)
 
   const getFruits = array => array.filter(item => item.type === 'fruit')
   const getVegetables = array => array.filter(item => item.type === 'vegetable')
+  const sortByAbc = array => array.sort((a, b) => (a.name > b.name ? 1 : -1))
+  const sortByPriceIncreasing = array =>
+    array.sort((a, b) => (a.price > b.price ? 1 : -1))
+  const sortByPriceDecreasing = array =>
+    array.sort((a, b) => (a.price > b.price ? -1 : 1))
 
-  const sortByAbc = array => {
-    console.log('ARRAY: ', array)
-    let itemNames = []
-    array.forEach(item => {
-      itemNames.push(item.name)
-    })
-    console.log('ITEM NAME ONLY: ', itemNames)
-    const sortedNames = itemNames.sort()
-    console.log('SORTED ITEM NAMES: ', sortedNames)
-
-    sortedNames.forEach(name => {
-      array.forEach(item)
-    })
-
-    //this needs to return an array of items i want to deispay.
-    return array
-  }
-
-  //??? shall i keep it as a standalone variable or use statehook for this too??? i have seen it this was in one of the previous excercises completed.
   let itemsToDisplay = storeItems
-  console.log('STORE ITEMS: ', storeItems)
 
   if (filterBy === 'fruit') itemsToDisplay = getFruits(itemsToDisplay)
   if (filterBy === 'vegetable') itemsToDisplay = getVegetables(itemsToDisplay)
   if (filterBy === 'all') itemsToDisplay = storeItems
-
   if (sortBy === 'abc') itemsToDisplay = sortByAbc(itemsToDisplay)
+  if (sortBy === 'priceIncreasing')
+    itemsToDisplay = sortByPriceIncreasing(itemsToDisplay)
+  if (sortBy === 'priceDecreasing')
+    itemsToDisplay = sortByPriceDecreasing(itemsToDisplay)
 
   return (
     <header id="store">
